e5b6e5ca437eee7aebf2d77fe3ffa99a7b91141a
Federico Ravasio <ravasio.federico@gmail.com>
2025-11-26 16:02:42 +0000

    fix: improve display of enums in clap's help

--- build.rs
+++ build.rs
@@ -4,6 +4,10 @@ use std::{
     fs::{self, File},
     path::Path,
 };
+use syn::{
+    ItemEnum,
+    visit_mut::{self, VisitMut},
+};
 
 fn main() {
     generate("aerocloud");
@@ -18,11 +22,29 @@ fn generate(name: &str) {
     let mut generator = Generator::default();
 
     let tokens = generator.generate_tokens(&spec).unwrap();
-    let ast = syn::parse2(tokens).unwrap();
-    let content = prettyplease::unparse(&ast);
+    let mut ast = syn::parse2(tokens).unwrap();
+    AddClapValueEnum.visit_file_mut(&mut ast);
 
     let mut out_file = Path::new(&env::var("OUT_DIR").unwrap()).to_path_buf();
     out_file.push(format!("codegen_{name}.rs"));
 
+    let content = prettyplease::unparse(&ast);
     fs::write(out_file, content).unwrap();
+}
+
+struct AddClapValueEnum;
+
+impl VisitMut for AddClapValueEnum {
+    fn visit_item_enum_mut(&mut self, node: &mut ItemEnum) {
+        if let "SimulationsV6ListStatus"
+        | "SimulationsV7ListStatus"
+        | "SimulationQuality"
+        | "ProjectStatus" = node.ident.to_string().as_str()
+        {
+            node.attrs
+                .push(syn::parse_quote! { #[derive(clap::ValueEnum)] });
+        }
+
+        visit_mut::visit_item_enum_mut(self, node);
+    }
 }

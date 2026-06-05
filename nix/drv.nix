3db0baa10d9c0decdcadd35336a91d80e9310c5c
Federico Ravasio <ravasio.federico@gmail.com>
2026-06-05 14:39:28 +0200

    upgrade(build): bump deps

--- nix/drv.nix
+++ nix/drv.nix
@@ -38,14 +38,6 @@
     inherit src;
 
     strictDeps = true;
-
-    buildInputs =
-      []
-      ++ lib.optionals stdenv.isDarwin [
-        libiconv
-        darwin.apple_sdk.frameworks.Security
-        darwin.apple_sdk.frameworks.SystemConfiguration
-      ];
   };
 
   cargoArtifacts = craneLib.buildDepsOnly commonArgs;

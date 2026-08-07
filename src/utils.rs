1e1b8e5f1313a1854350df6cbb448113ce4f41c7
Federico Ravasio <ravasio.federico@gmail.com>
2026-08-07 09:36:14 +0000

    upgrade(Build): bump deps, fix `comfy-table` breaking changes

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,10 @@
-use comfy_table::{
-    ContentArrangement, Table, modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL,
-};
+use comfy_table::{ContentArrangement, Table, presets::UTF8_FULL};
 
 pub fn new_dynamic_table() -> Table {
     let mut t = Table::new();
 
     t.set_content_arrangement(ContentArrangement::Dynamic)
-        .load_preset(UTF8_FULL)
-        .apply_modifier(UTF8_ROUND_CORNERS);
+        .load_style(UTF8_FULL.with_rounded_corners());
+
     t
 }
